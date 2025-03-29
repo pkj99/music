6cfb5d17d204ac856c27bb5440b1129f4e93a55b
unknown <james.peng.pkj@gmail.com>
2025-03-29 16:00:11 +0800

    make it better

--- js/player.js
+++ js/player.js
@@ -436,7 +436,6 @@ function play(music) {
     if (music.url.includes('/kw/')) {
         KuwoUrl(music.url_id,function(mp3Url){
             try {
-                alert(mp3Url);
                 rem.audio[0].pause();
                 rem.audio.attr('src', mp3Url);
                 rem.audio[0].play();
